a6e974f13bc01e2eb5b220b86aac6c722a546086
xkaixiong <xkaixiong@qq.com>
2020-10-27 17:16:08 +0800

    deploy

---
+++ precache-manifest.4d74ad8892e48e05e93641afb04ec12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d30662a9b782402e46f8e5034391f6f",
+    "url": "/goldfishaccount-website/index.html"
+  },
+  {
+    "revision": "414c764d143d82b14e4f",
+    "url": "/goldfishaccount-website/static/css/main.7bcb4a8d.chunk.css"
+  },
+  {
+    "revision": "c1b57a9e8b925d8afe28",
+    "url": "/goldfishaccount-website/static/js/2.63b50ecb.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/goldfishaccount-website/static/js/2.63b50ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414c764d143d82b14e4f",
+    "url": "/goldfishaccount-website/static/js/main.eb80c672.chunk.js"
+  },
+  {
+    "revision": "2d89d2e403bf4457d51d",
+    "url": "/goldfishaccount-website/static/js/runtime-main.19456858.js"
+  }
+]);
